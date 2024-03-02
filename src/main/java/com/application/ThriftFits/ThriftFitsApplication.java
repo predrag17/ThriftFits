5b169171c8fbf206b619b6dc7c5fdb7d28b851f4
predrag17 <sterjoskipredragit@gmail.com>
2024-03-02 00:36:59 +0000

    Initial commit

---
+++ src/main/java/com/application/ThriftFits/ThriftFitsApplication.java
@@ -0,0 +1,13 @@
+package com.application.ThriftFits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThriftFitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThriftFitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/ThriftFits/ThriftFitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.ThriftFits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThriftFitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
